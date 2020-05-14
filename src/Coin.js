4485ce4392887255d2afa8767d89dc6af135af2c
Mariya <mariya.podosinova@gmail.com>
2020-05-14 12:38:19 +0100

    first commit

---
+++ src/Coin.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+class Coin extends Component {}
+
+export default Coin;
---
+++ src/CoinContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CoinContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lets Flip a coin</h1>
+      </div>
+    );
+  }
+}
+
+export default CoinContainer;

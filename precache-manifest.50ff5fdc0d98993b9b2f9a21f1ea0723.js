b42d2ef4084b8e04567e0a8d1eef4cea0017effe
Mariya <mariya.podosinova@gmail.com>
2020-05-14 15:22:13 +0100

    Updates

---
+++ precache-manifest.50ff5fdc0d98993b9b2f9a21f1ea0723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec0dbb8ae11e4abada7825193aac04a",
+    "url": "/coin_flipper/index.html"
+  },
+  {
+    "revision": "531f6c8b5e52e90042d7",
+    "url": "/coin_flipper/static/css/main.5c1749cf.chunk.css"
+  },
+  {
+    "revision": "83af90c54f5037def5e7",
+    "url": "/coin_flipper/static/js/2.8fd21e1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coin_flipper/static/js/2.8fd21e1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531f6c8b5e52e90042d7",
+    "url": "/coin_flipper/static/js/main.eba75d98.chunk.js"
+  },
+  {
+    "revision": "1038cc14caeb2f601993",
+    "url": "/coin_flipper/static/js/runtime-main.12ea8001.js"
+  }
+]);
